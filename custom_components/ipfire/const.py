0043fc21b204b85ed4cc443f698c5c1a10f93e83
Frank Mainz <frank@cybermainzel.de>
2026-08-31 23:49:24 +0200

    Update const.py

--- custom_components/ipfire/const.py
+++ custom_components/ipfire/const.py
@@ -1,4 +1,14 @@
+from __future__ import annotations
+
 DOMAIN = "ipfire"
+
+DATA_COORDINATOR = "coordinator"
+
+CONF_URL = "url"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_VERIFY_SSL = "verify_ssl"
+CONF_SCAN_INTERVAL = "scan_interval"
 
 DEFAULT_URL = "https://ipfire.local:444"
 DEFAULT_SCAN_INTERVAL = 30
@@ -7,11 +17,3 @@ MIN_SCAN_INTERVAL = 5
 MAX_SCAN_INTERVAL = 60
 
 SPEED_PATH = "/cgi-bin/speed.cgi"
-
-CONF_URL = "url"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-CONF_VERIFY_SSL = "verify_ssl"
-CONF_SCAN_INTERVAL = "scan_interval"
-
-DATA_COORDINATOR = "coordinator"
